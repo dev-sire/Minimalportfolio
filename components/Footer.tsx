8b6de15474f0dcc8ef1ed53014cd5fd23d2da475
Aman Shahid <shahidamaan2019@gmail.com>
2025-01-08 23:39:55 +0500

    new portfolio

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,12 +1,10 @@
 import { FaLocationArrow } from "react-icons/fa6";
-
 import { socialMedia } from "@/data";
 import MagicButton from "./MagicButton";
 
 const Footer = () => {
   return (
     <footer className="w-full pt-20 pb-10" id="contact">
-      {/* background grid */}
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
         <img
           src="/footer-grid.svg"
